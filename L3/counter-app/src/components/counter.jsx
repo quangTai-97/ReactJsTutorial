dc7d99493feb10ad09b21c5e9d4bba0310895be1
quangtai-97 <dqtai97@gmail.com>
2021-04-27 17:01:19 +0700

    commit

--- L3/counter-app/src/components/counter.jsx
+++ L3/counter-app/src/components/counter.jsx
@@ -3,35 +3,39 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    imgaeUrl: "https://picsum.photos/200",
-    tags: ["tag1", "tag2", "tag3"],
+
   };
 
+  
+  handleIncrement = () =>{
+
+   this.setState({count: this.state.count + 1})
+  }
+
+  
   render() {
+  
     return (
       <div>
-        <span className={this.getBadgeClasses()}>{this.formatcount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map((tag, index) => (
-            <li key={index}>{tag}</li>
-          ))}
-        </ul>
+       <span className ={this.getBadgeClasses()}>{this.formatCount()}</span>
+       <button 
+       onClick={this.handleIncrement}
+       className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
-
-  getBadgeClasses() {
+  
+  getBadgeClasses(){
     let classes = "badge m-2 badge-";
     classes += this.state.count === 0 ? "warning" : "primary";
     return classes;
   }
-
-  formatcount() {
-    const { count } = this.state;
-
-    return count === 0 ? "Zero" : count;
+  formatCount()
+  {
+    const {count} = this.state;
+    return (count === 0 ? "Zero" : count);
   }
+
 }
 
 export default Counter;
